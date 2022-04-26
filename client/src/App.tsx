978bc9fa66aab60bc7a97a3533f3b8472b3047ea
amira ZOUAK <zouak.amira@gmail.com>
2022-04-26 17:06:29 +0200

    changed title after port changed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with CD </h1>
+          <h1>Wilders Book with CD port 8001 </h1>
         </Container>
       </Header>
       <Container>
