491a1d4f850de0959712b431ca526ec89a90b7ba
amira ZOUAK <zouak.amira@gmail.com>
2022-04-26 16:47:03 +0200

    changed title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with CD webhook</h1>
+          <h1>Wilders Book with CD after docker restart</h1>
         </Container>
       </Header>
       <Container>
