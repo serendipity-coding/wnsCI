c1e82ce16eae6d7dada60c68ef10058e3853859b
amira ZOUAK <zouak.amira@gmail.com>
2022-04-26 16:13:46 +0200

    changed title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with CD</h1>
+          <h1>Wilders Book with CD with webhook</h1>
         </Container>
       </Header>
       <Container>
