8748cf954bb20d6e139e67f58e623f2e5d4699f3
amira ZOUAK <zouak.amira@gmail.com>
2022-04-26 15:14:42 +0200

    add script fetch

---
+++ fetch-and-deploy.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+docker-compose -f docker-compose.prod.yml down && \
+docker-compose -f docker-compose.prod.yml pull && \
+GATEWAY_PORT=8001 docker-compose -f docker-compose.prod.yml up -d;
